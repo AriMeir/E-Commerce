c329bf5ea8f089de0f3fee052ec15b11babd9bd2
Ari Meir <arimeir95@example.com>
2025-11-17 20:22:19 +0200

    categories path

--- Client/e-commerce/app/components/Categories.tsx
+++ Client/e-commerce/app/components/Categories.tsx
@@ -1,7 +1,87 @@
 "use client"
+import {
+  Footprints,
+  Glasses,
+  Briefcase,
+  Shirt,
+  ShoppingBasket,
+  Hand,
+  Venus,
+} from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+const categories = [
+  {
+    name: "All",
+    icon: <ShoppingBasket className="w-4 h-4" />,
+    slug: "all",
+  },
+  {
+    name: "T-shirts",
+    icon: <Shirt className="w-4 h-4" />,
+    slug: "t-shirts",
+  },
+  {
+    name: "Shoes",
+    icon: <Footprints className="w-4 h-4" />,
+    slug: "shoes",
+  },
+  {
+    name: "Accessories",
+    icon: <Glasses className="w-4 h-4" />,
+    slug: "accessories",
+  },
+  {
+    name: "Bags",
+    icon: <Briefcase className="w-4 h-4" />,
+    slug: "bags",
+  },
+  {
+    name: "Dresses",
+    icon: <Venus className="w-4 h-4" />,
+    slug: "dresses",
+  },
+  {
+    name: "Jackets",
+    icon: <Shirt className="w-4 h-4" />,
+    slug: "jackets",
+  },
+  {
+    name: "Gloves",
+    icon: <Hand className="w-4 h-4" />,
+    slug: "gloves",
+  },
+];
+
+
+
+
 const Categories = () => {
+  const searchParams = useSearchParams()
+  const rounter = useRouter()
+  const pathName = usePathname()
+  const selectedCategory = searchParams.get("category")
+  console.log(selectedCategory)
+
+  const handleChange = (value: string | null) => {
+    const params = new URLSearchParams(searchParams)
+    params.set("category", value || 'all' )
+    rounter.push(`${pathName}?${params.toString()}`)
+  }
+
+
   return (
-    <div>Categories</div>
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 bg-gray-100 rounded-lg p-2 mb-4 text-sm}">
+      {categories.map(category => (
+        <div className={`flex items-center justify-center gap-2 rounded-2xl cursor-pointer px-2 py-1 round-md ${category.slug == selectedCategory ? "bg-white" : "text-gray-500"}`} 
+          key={category.name}
+          onClick={() => handleChange(category.slug)}>
+          {category.icon}
+          {category.name}
+        </div>
+      ))}
+    </div>
+
   )
 }
 
--- Client/e-commerce/app/components/ProductList.tsx
+++ Client/e-commerce/app/components/ProductList.tsx
@@ -119,7 +119,9 @@ const ProductList = () => {
     <div className="w-full ">
         <Categories/>
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3 gap-12">
-            {products.map(product => <ProductCard/>)}
+            {products.map(product =>
+                 <ProductCard key={product.id} product={product}/>)
+                 }
         </div>
         
     </div>
